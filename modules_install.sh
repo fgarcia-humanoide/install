9d9177e7f1a74f31e55576f00f34021994ded01a
Fernando García Hansen <fgarcia@humanoide.es>
2017-07-31 18:42:42 +0200

    Update modules_install.sh

--- modules_install.sh
+++ modules_install.sh
@@ -35,6 +35,7 @@ sudo git clone --branch 10.0 https://github.com/OCA/bank-statement-import.git $O
 echo -e "\n---- Create some links ----"
 sudo ln -s $OE_HOME/account-financial-reporting/account_export_csv $OE_HOME/custom/addons/
 sudo ln -s $OE_HOME/account-financial-reporting/account_move_line_report_xls $OE_HOME/custom/addons/
+sudo ln -s $OE_HOME/account-financial-reporting/account_tax_balance $OE_HOME/custom/addons/
 sudo ln -s $OE_HOME/account-financial-tools/account_chart_update $OE_HOME/custom/addons/
 sudo ln -s $OE_HOME/account-financial-tools/account_invoice_constraint_chronology $OE_HOME/custom/addons/
 sudo ln -s $OE_HOME/account-financial-tools/account_invoice_currency $OE_HOME/custom/addons/
