7e196c40c05032d260fc5a08741a67f3b3de2dca
Fernando García Hansen <fgarcia@humanoide.es>
2017-07-31 18:35:39 +0200

    Update modules_install.sh

--- modules_install.sh
+++ modules_install.sh
@@ -49,6 +49,7 @@ sudo ln -s $OE_HOME/bank-payment/account_banking_payment_transfer/ $OE_HOME/cust
 sudo ln -s $OE_HOME/bank-payment/account_banking_sepa_direct_debit/ $OE_HOME/custom/addons/
 sudo ln -s $OE_HOME/bank-payment/account_direct_debit/ $OE_HOME/custom/addons/
 sudo ln -s $OE_HOME/bank-payment/account_payment_partner $OE_HOME/custom/addons/
+sudo ln -s $OE_HOME/bank-payment/account_payment_mode $OE_HOME/custom/addons/
 sudo ln -s $OE_HOME/bank-statement-import/account_bank_statement_import/ $OE_HOME/custom/addons/
 sudo ln -s $OE_HOME/knowledge/attachment_preview/ $OE_HOME/custom/addons/
 sudo ln -s $OE_HOME/knowledge/attachments_to_filesystem/ $OE_HOME/custom/addons/
