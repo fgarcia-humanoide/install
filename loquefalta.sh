d4a761acdfd82f6e71379bb634fc13e1a87a37eb
Fernando García Hansen <fgarcia@humanoide.es>
2017-12-06 11:36:33 +0100

    Update loquefalta.sh

--- loquefalta.sh
+++ loquefalta.sh
@@ -1,1 +1,11 @@
-# install
+#!/bin/bash
+################################################################################
+# metamos lo que falta
+################################################################################
+ 
+sudo pip install --upgrade pip
+sudo pip install setuptools
+sudo pip install unidecode
+sudo pip install unicodecsv
+sudo pip install xlsxwriter 
+sudo pip install babel 
