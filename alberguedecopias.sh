bae7a3e3d8061e27d5642f6c1d77d0f1594ec2f5
Fernando García Hansen <fgarcia@humanoide.es>
2017-12-08 12:11:47 +0100

    Update and rename loquefalta.sh to alberguedecopias.sh

---
+++ alberguedecopias.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+################################################################################
+# preparemos un albergue de copias
+################################################################################
+ 
+sudo mkdir /home/postgres
+chown -R postgres:postgres /home/postgres
+
