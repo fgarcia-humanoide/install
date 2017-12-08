aafa4d6ad9fa66a05c0baeb6ce3b9136beec7b67
Fernando García Hansen <fgarcia@humanoide.es>
2017-12-08 16:57:37 +0100

    Update scriptdecopias.sh

--- scriptdecopias.sh
+++ scriptdecopias.sh
@@ -3,5 +3,8 @@
 # script que hace copias
 ################################################################################
  
-sudo mkdir /home/postgres
-chown -R postgres:postgres /home/postgres
+sudo mkdir /var/scripts
+wget https://raw.githubusercontent.com/fgarcia-humanoide/install/10/clean_backup.sh /var/scripts
+wget https://raw.githubusercontent.com/fgarcia-humanoide/install/10/odoo_backup.sh /var/scripts
+chown postgres:postgres /var/scripts/clean_backup.sh
+chown postgres:postgres /var/scripts/odoo_backup.sh
