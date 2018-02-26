2f27bf380a69cdda65e4e077cc30184896a223fa
Fernando García Hansen <fgarcia@humanoide.es>
2018-02-26 12:18:17 +0100

    Update modules_install.sh

--- modules_install.sh
+++ modules_install.sh
@@ -26,6 +26,7 @@ sudo git clone --branch 11.0 https://github.com/OCA/project.git $OE_HOME/project
 sudo git clone --branch 11.0 https://github.com/OCA/crm.git $OE_HOME/crm
 sudo git clone --branch 11.0 https://github.com/OCA/account-closing.git $OE_HOME/account-closing
 sudo git clone --branch 11.0 https://github.com/fgarcia-humanoide/supermodulo.git $OE_HOME/supermodulo
+sudo git clone --branch 11.0 https://github.com/OCA/server-ux.git $OE_HOME/server-ux
 
 
 
