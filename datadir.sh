25596e45fe571816634a81ef3fa903129d8db7fb
Fernando García Hansen <fgarcia@humanoide.es>
2018-04-22 11:58:52 +0200

    Update datadir.sh

--- datadir.sh
+++ datadir.sh
@@ -9,4 +9,5 @@ sudo mv /opt/odoo/.local/share/Odoo /opt/datadir
 sudo chown -R odoo:odoo /opt/datadir
 sed -i 's+/opt/odoo/.local/share/Odoo+/opt/datadir/Odoo+g' /etc/odoo-server.conf
 sed -i 's+logrotate = False+logrotate = True+g' /etc/odoo-server.conf
+sed -i 's+proxy_mode = False+proxy_mode = True+g' /etc/odoo-server.conf
 sudo service odoo-server restart
