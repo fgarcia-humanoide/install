28aa1169054872e87ef7d2c4382376567f75e1f4
Fernando García Hansen <fgarcia@humanoide.es>
2018-04-22 10:51:18 +0200

    Update datadir.sh

--- datadir.sh
+++ datadir.sh
@@ -5,7 +5,7 @@
 ################################################################################
 
 sudo mkdir /opt/datadir
-sudo mv /opt/odoo/.local/share/Odoo /opt/datadir/Odoo
+sudo mv /opt/odoo/.local/share/Odoo /opt/datadir
 sudo chown -R odoo:odoo /opt/datadir
 sudo echo 'data_dir = /opt/datadir/Odoo'>> /etc/odoo-server.conf
 sudo echo 'logrotate = True'>> /etc/odoo-server.conf
