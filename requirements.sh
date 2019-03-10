3643ec43df3d3ce6020152b51a38070d052e59e8
Fernando García Hansen <fgarcia@humanoide.es>
2019-03-10 12:41:17 +0100

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -1,34 +1,9 @@
-cd /opt/odoo/account-closing
-pip3 install -r requirements.txt
-
-
 cd /opt/odoo/account-financial-reporting
 pip3 install -r requirements.txt
 
 
 cd /opt/odoo/account-financial-tools
 pip3 install -r requirements.txt
-
-
-cd /opt/odoo/account-invoice-reporting
-pip3 install -r requirements.txt
-
-
-cd /opt/odoo/account-invoicing
-pip3 install -r requirements.txt
-
-
-cd /opt/odoo/account-payment
-pip3 install -r requirements.txt
-
-
-cd /opt/odoo/backend_theme
-pip3 install -r requirements.txt
-
-
-cd /opt/odoo/bank-payment
-pip3 install -r requirements.txt
-
 
 cd /opt/odoo/bank-statement-import
 pip3 install -r requirements.txt
@@ -37,46 +12,11 @@ pip3 install -r requirements.txt
 cd /opt/odoo/community-data-files
 pip3 install -r requirements.txt
 
-
-cd /opt/odoo/contract
-pip3 install -r requirements.txt
-
-
-cd /opt/odoo/crm
-pip3 install -r requirements.txt
-
-
-cd /opt/odoo/custom
-pip3 install -r requirements.txt
-
-
-cd /opt/odoo/knowledge
-pip3 install -r requirements.txt
-
-
 cd /opt/odoo/l10n-spain
 pip3 install -r requirements.txt
 
-
-cd /opt/odoo/mis-builder
-pip3 install -r requirements.txt
-
-
 cd /opt/odoo/odoo-server
 pip3 install -r requirements.txt
-
-
-cd /opt/odoo/partner-contact
-pip3 install -r requirements.txt
-
-
-cd /opt/odoo/pos
-pip3 install -r requirements.txt
-
-
-cd /opt/odoo/project
-pip3 install -r requirements.txt
-
 
 cd /opt/odoo/purchase-workflow
 pip3 install -r requirements.txt
@@ -89,26 +29,8 @@ pip3 install -r requirements.txt
 cd /opt/odoo/reporting-engine
 pip3 install -r requirements.txt
 
-
-cd /opt/odoo/sale-workflow
-pip3 install -r requirements.txt
-
-
-cd /opt/odoo/server-brand
-pip3 install -r requirements.txt
-
-
 cd /opt/odoo/server-tools
 pip3 install -r requirements.txt
-
-
-cd /opt/odoo/server-ux
-pip3 install -r requirements.txt
-
-
-cd /opt/odoo/stock-logistics-workflow
-pip3 install -r requirements.txt
-
 
 cd /opt/odoo/web
 pip3 install -r requirements.txt
