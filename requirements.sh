4907950dacd8d7b0f36c2a60b0cc908bf2233095
Fernando García Hansen <fgarcia@humanoide.es>
2019-03-10 12:20:20 +0100

    Create requirements.sh

---
+++ requirements.sh
@@ -0,0 +1,114 @@
+cd /opt/odoo/account-closing
+pip3 install -r requirements.txt
+
+
+cd /opt/odoo/account-financial-reporting
+pip3 install -r requirements.txt
+
+
+cd /opt/odoo/account-financial-tools
+pip3 install -r requirements.txt
+
+
+cd /opt/odoo/account-invoice-reporting
+pip3 install -r requirements.txt
+
+
+cd /opt/odoo/account-invoicing
+pip3 install -r requirements.txt
+
+
+cd /opt/odoo/account-payment
+pip3 install -r requirements.txt
+
+
+cd /opt/odoo/backend_theme
+pip3 install -r requirements.txt
+
+
+cd /opt/odoo/bank-payment
+pip3 install -r requirements.txt
+
+
+cd /opt/odoo/bank-statement-import
+pip3 install -r requirements.txt
+
+
+cd /opt/odoo/community-data-files
+pip3 install -r requirements.txt
+
+
+cd /opt/odoo/contract
+pip3 install -r requirements.txt
+
+
+cd /opt/odoo/crm
+pip3 install -r requirements.txt
+
+
+cd /opt/odoo/custom
+pip3 install -r requirements.txt
+
+
+cd /opt/odoo/knowledge
+pip3 install -r requirements.txt
+
+
+cd /opt/odoo/l10n-spain
+pip3 install -r requirements.txt
+
+
+cd /opt/odoo/mis-builder
+pip3 install -r requirements.txt
+
+
+cd /opt/odoo/odoo-server
+pip3 install -r requirements.txt
+
+
+cd /opt/odoo/partner-contact
+pip3 install -r requirements.txt
+
+
+cd /opt/odoo/pos
+pip3 install -r requirements.txt
+
+
+cd /opt/odoo/project
+pip3 install -r requirements.txt
+
+
+cd /opt/odoo/purchase-workflow
+pip3 install -r requirements.txt
+
+
+cd /opt/odoo/queue
+pip3 install -r requirements.txt
+
+
+cd /opt/odoo/reporting-engine
+pip3 install -r requirements.txt
+
+
+cd /opt/odoo/sale-workflow
+pip3 install -r requirements.txt
+
+
+cd /opt/odoo/server-brand
+pip3 install -r requirements.txt
+
+
+cd /opt/odoo/server-tools
+pip3 install -r requirements.txt
+
+
+cd /opt/odoo/server-ux
+pip3 install -r requirements.txt
+
+
+cd /opt/odoo/stock-logistics-workflow
+pip3 install -r requirements.txt
+
+
+cd /opt/odoo/web
+pip3 install -r requirements.txt
