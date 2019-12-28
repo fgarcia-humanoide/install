1c126d6b6ad498436a01845ff60476374bc96baf
Fernando García Hansen <fgarcia@humanoide.es>
2019-12-28 07:10:26 +0100

    Update concatenar.sh

--- concatenar.sh
+++ concatenar.sh
@@ -1,5 +1,6 @@
-
-
-
+#!/bin/bash
+################################################################################
+# script que hace copias
+################################################################################
 
 ./makeswap.sh && ./postgres11.sh && ./odoo_install.sh && ./modules_install.sh && ./webmin_install.sh && ./alberguedecopias.sh && ./letsencrypt.sh && ./scriptdecopias.sh && ./requirements.sh
