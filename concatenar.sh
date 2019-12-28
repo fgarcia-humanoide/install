71983bb9f09904e7fe5b5d6c40c88dcfb03069ff
Fernando García Hansen <fgarcia@humanoide.es>
2019-12-28 07:09:49 +0100

    Create concatenar.sh

---
+++ concatenar.sh
@@ -0,0 +1,5 @@
+
+
+
+
+./makeswap.sh && ./postgres11.sh && ./odoo_install.sh && ./modules_install.sh && ./webmin_install.sh && ./alberguedecopias.sh && ./letsencrypt.sh && ./scriptdecopias.sh && ./requirements.sh
