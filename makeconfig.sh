36dfb7f487d619ecc28fa228fe2b46bdbf01bb5e
Fernando García Hansen <fgarcia@humanoide.es>
2020-07-27 10:32:10 +0200

    Update makeconfig.sh

--- makeconfig.sh
+++ makeconfig.sh
@@ -2,7 +2,8 @@
 ################################################################################
 # Script for creating a proper config file
 ################################################################################
- 
-sudo /opt/odoo/odoo-server python3 --save --config /etc/odoo-server.conf
+sudo service odoo-server stop 
+cd /opt/odoo/odoo-server
+python3 odoo-bin --save --config /etc/odoo-server.conf
 sudo chmod 640 /etc/odoo-server.conf
 sudo chown odoo:odoo /etc/odoo-server.conf
