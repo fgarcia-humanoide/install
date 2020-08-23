3ce79115c7e7a37f3d815e863f489e90c1248baa
Fernando García Hansen <fgarcia@humanoide.es>
2020-08-23 12:55:41 +0200

    Update cambiacrontab.sh

--- cambiacrontab.sh
+++ cambiacrontab.sh
@@ -1,5 +1,4 @@
-echo '0  3    * * 5   root    /opt/letsencrypt/letsencrypt-auto renew >> /var/log/le-renew.log'>> /etc/crontab
-echo '5  3    * * 5   root    service apache2 restart'>> /etc/crontab
+echo '0  3    * * 5   root    certbot renew --pre-hook "service nginx stop" --post-hook "service nginx start" >> /var/log/le-renew.log'>> /etc/crontab
 echo '0 13    * * *   postgres /var/scripts/odoo_backup.sh'>> /etc/crontab
 echo '0 20    * * *   postgres /var/scripts/odoo_backup.sh'>> /etc/crontab
 echo '0 21    * * *   postgres /var/scripts/clean_backup.sh'>> /etc/crontab
