9ccd7db711527778d166392c7e6ad7f87b831516
Fernando García Hansen <fgarcia@humanoide.es>
2020-08-31 23:05:32 +0200

    Update odoo_install.sh

--- odoo_install.sh
+++ odoo_install.sh
@@ -35,7 +35,7 @@ OE_SUPERADMIN="admin"
 GENERATE_RANDOM_PASSWORD="False"
 OE_CONFIG="${OE_USER}-server"
 # Set the website name
-WEBSITE_NAME="tiendadeprueba.humanoide.es"
+WEBSITE_NAME="vigotoner.humanoide.es"
 # Set the default Odoo longpolling port (you still have to use -c /etc/odoo-server.conf for example to use this.)
 LONGPOLLING_PORT="8072"
 # Set to "True" to install certbot and have ssl enabled, "False" to use http
