1c3ca15dac287072375928ca9cb375dbab64a356
Fernando García Hansen <fgarcia@humanoide.es>
2020-09-10 12:37:44 +0200

    Update concatenar.sh

--- concatenar.sh
+++ concatenar.sh
@@ -3,4 +3,4 @@
 # script que hace copias
 ################################################################################
 
-./makeswap.sh && ./postgres11.sh && ./odoo_install.sh && ./modules_install.sh && ./webmin_install.sh && ./alberguedecopias.sh && ./scriptdecopias.sh && ./requirements.sh && ./cambiacrontab.sh
+./makeswap.sh && ./postgres11.sh && ./odoo_install.sh && ./modules_install.sh && ./webmin_install.sh && ./alberguedecopias.sh && ./scriptdecopias.sh && ./cambiacrontab.sh && ./makeconfig.sh && ./datadir.sh && ./cambiaruta.sh && . ./cambiadir.sh && ./requirements_oca.sh
