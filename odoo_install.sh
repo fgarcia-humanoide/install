b2e143a2d41898866f39a441055415432d1391c2
Fernando García Hansen <fgarcia@humanoide.es>
2021-05-01 00:23:59 +0200

    Update odoo_install.sh

--- odoo_install.sh
+++ odoo_install.sh
@@ -49,7 +49,7 @@ ADMIN_EMAIL="fgarcia@humanoide.es"
 ## https://github.com/odoo/odoo/wiki/Wkhtmltopdf ):
 ## https://www.odoo.com/documentation/13.0/setup/install.html#debian-ubuntu
 
-WKHTMLTOX_X64=https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.5/wkhtmltox_0.12.5-1.trusty_amd64.deb
+WKHTMLTOX_X64=https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox_0.12.6-1.focal_amd64.deb
 WKHTMLTOX_X32=https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.5/wkhtmltox_0.12.5-1.trusty_i386.deb
 #--------------------------------------------------
 # Update Server
