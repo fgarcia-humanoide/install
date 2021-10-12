cc185806785e26eaac3e5c050b41c39299f275d2
Fernando García Hansen <fgarcia@humanoide.es>
2021-10-12 08:48:25 +0200

    Update cambiacrontab.sh

--- cambiacrontab.sh
+++ cambiacrontab.sh
@@ -1,6 +1,5 @@
 echo '0  3    * * 5   root    certbot renew --pre-hook "service nginx stop" --post-hook "service nginx start" >> /var/log/le-renew.log'>> /etc/crontab
 echo '0 13    * * *   postgres /var/scripts/odoo_backup.sh'>> /etc/crontab
 echo '0 20    * * *   postgres /var/scripts/odoo_backup.sh'>> /etc/crontab
-echo '0 20    * * *   cp /etc/odoo-server.conf /opt/odoo
 echo '0 21    * * *   postgres /var/scripts/clean_backup.sh'>> /etc/crontab
 echo '#'>> /etc/crontab
