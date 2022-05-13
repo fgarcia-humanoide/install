8d20ab0f071201637ae9bd6c202907a231d3b3a8
Fernando García Hansen <fgarcia@humanoide.es>
2022-05-13 12:58:28 +0200

    Update instalazip.sh

--- instalazip.sh
+++ instalazip.sh
@@ -1,3 +1,3 @@
 sudo apt-get install p7zip-full
-7z x ls /root/install/legion_enterprise_theme-15.0.1.0.1.zip
-7z x ls /root/install/show_db_name-15.0.1.0.0.zip
+7z x /root/install/legion_enterprise_theme-15.0.1.0.1.zip
+7z x /root/install/show_db_name-15.0.1.0.0.zip
