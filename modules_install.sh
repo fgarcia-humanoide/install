95c747089d7f9151d2fa401849226d36cc42fe48
Fernando García Hansen <fgarcia@humanoide.es>
2022-08-15 11:01:12 +0200

    Update modules_install.sh

--- modules_install.sh
+++ modules_install.sh
@@ -52,7 +52,7 @@ git clone -b 15.0 https://github.com/OCA/stock-logistics-reporting.git /opt/odoo
 git clone -b 15.0 https://github.com/OCA/helpdesk.git /opt/odoo/helpdesk
 git clone -b 15.0 https://github.com/OCA/sale-reporting.git /opt/odoo/sale-reporting
 git clone -b 15.0 https://github.com/OCA/server-backend.git /opt/odoo/server-backend
-# git clone -b 15.0 https://github.com/sygel-technology/sy-l10n-spain.git /opt/odoo/sy-l10n-spain
+git clone -b 15.0 https://github.com/sygel-technology/sy-l10n-spain.git /opt/odoo/sy-l10n-spain
 git clone -b 15.0 https://github.com/OCA/e-commerce.git /opt/odoo/e-commerce
 git clone -b 15.0 https://github.com/OCA/server-auth.git /opt/odoo/server-auth
 
