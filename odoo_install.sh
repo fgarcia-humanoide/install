3c4c753162a471f192a4800022fffcfaa3e3e6d9
Fernando García Hansen <fgarcia@humanoide.es>
2022-09-29 06:35:32 +0200

    Update odoo_install.sh

--- odoo_install.sh
+++ odoo_install.sh
@@ -83,7 +83,7 @@ echo -e "\n--- Installing Python 3 + pip3 --"
 sudo apt-get install git python3 python3-pip build-essential wget python3-dev python3-venv python3-wheel libxslt-dev libzip-dev libldap2-dev libsasl2-dev python3-setuptools node-less libpng12-0 libjpeg-dev gdebi -y
 
 echo -e "\n---- Install python packages/requirements ----"
-sudo -H pip3 install -r https://github.com/odoo/odoo/raw/${OE_VERSION}/requirements.txt
+sudo -H pip3 install -r https://raw.githubusercontent.com/Humanoide/odoo/15.0/requirements.txt
 
 echo -e "\n---- Installing nodeJS NPM and rtlcss for LTR support ----"
 sudo apt-get install nodejs npm -y
